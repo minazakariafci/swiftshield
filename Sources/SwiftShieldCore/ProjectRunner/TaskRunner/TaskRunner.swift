dc498ab15cdc842b2641b35ff06ed7afdca4f3e5
Mina Zakaria <Mina.zakaria@alahlymomkn.com>
2024-08-19 22:09:07 +0300

    fix issue

--- Sources/SwiftShieldCore/ProjectRunner/TaskRunner/TaskRunner.swift
+++ Sources/SwiftShieldCore/ProjectRunner/TaskRunner/TaskRunner.swift
@@ -1,6 +1,6 @@
-#if os(macOS) || os(Linux)
 import Foundation
 
+#if os(macOS) || os(Linux)
 struct TaskRunner: TaskRunnerProtocol {
     func runTask(withCommand command: String, arguments: [String]) -> TaskRunnerOutput {
         let task = Process()
@@ -19,6 +19,13 @@ struct TaskRunner: TaskRunnerProtocol {
 
         let outputString = String(data: outdata, encoding: .utf8)
         return TaskRunnerOutput(output: outputString, terminationStatus: task.terminationStatus)
+    }
+}
+#else
+struct TaskRunner: TaskRunnerProtocol {
+    func runTask(withCommand command: String, arguments: [String]) -> TaskRunnerOutput {
+        // Provide a no-op or alternative implementation
+        return TaskRunnerOutput(output: "Not supported on this platform", terminationStatus: -1)
     }
 }
 #endif
